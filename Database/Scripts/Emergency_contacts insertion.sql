1733ca519bc7e8a922de1510491389f8d70065ef
Davina Gladys IRIBAGIZA MUTARUTINYA <72495685+Davina203g@users.noreply.github.com>
2025-12-07 12:19:34 +0200

    Create Emergency_contacts insertion.sql

---
+++ Database/Scripts/Emergency_contacts insertion.sql
@@ -0,0 +1,3 @@
+INSERT INTO EMERGENCY_CONTACT(contact_id, passenger_id, contact_name, relationship, contact_phone) VALUES (1, 1, 'Alice Muk', 'Sister', '250788111111');
+INSERT INTO EMERGENCY_CONTACT(contact_id, passenger_id, contact_name, relationship, contact_phone) VALUES (2, 2, 'Paul N', 'Husband', '250788222222');
+INSERT INTO EMERGENCY_CONTACT(contact_id, passenger_id, contact_name, relationship, contact_phone) VALUES (3, 3, 'Jeanette K', 'Mother', '250788333333');
